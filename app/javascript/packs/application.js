74326f2ef04b77075b51d0e01adff81e37dbf4fe
shes50103 <shes50103@gmail.com>
2021-01-23 11:23:18 +0800

    Bootstrap navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,10 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
