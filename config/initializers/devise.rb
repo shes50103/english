45ba567a32b296f0715cfd2d79cef246ddab3020
shes50103 <shes50103@gmail.com>
2021-03-14 15:48:49 +0800

    Code refactor

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -273,7 +273,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   # config.omniauth :facebook, Rails.application.credentials.facebook_client_id, Rails.application.credentials.facebook_client_secret, scope: "public_profile,email", info_fields: "email,name"
-  config.omniauth :google_oauth2, Rails.application.credentials.google[:client_id], Rails.application.credentials.google[:client_secret], {access_type: "offline", approval_prompt: ""}
+  config.omniauth :google_oauth2, Rails.application.credentials.dig(:google, :client_id), Rails.application.credentials.dig(:google, :client_secret), {access_type: "offline", approval_prompt: ""}
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
